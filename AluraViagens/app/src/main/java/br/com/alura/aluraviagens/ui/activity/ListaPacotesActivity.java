b26fb34692eb975f2b7f2e60b92a549b2b8c826e
Cloter Gonçalves Barbosa <cbarbosa@viceri.com.br>
2019-06-11 05:59:25 -0300

    Curso de Constraints Layout Finalizado

--- AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -1,7 +1,10 @@
 package br.com.alura.aluraviagens.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -10,6 +13,8 @@ import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.dao.PacoteDAO;
 import br.com.alura.aluraviagens.model.Pacote;
 import br.com.alura.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+import static br.com.alura.aluraviagens.ui.activity.PacoteActivityConstantes.CHAVE_PACOTE;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -29,8 +34,22 @@ public class ListaPacotesActivity extends AppCompatActivity {
     private void configuraLista() {
         ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
 
-        List<Pacote> pacotes = new PacoteDAO().lista();
+        final List<Pacote> pacotes = new PacoteDAO().lista();
 
         listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+
+        listaDePacotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int posicao, long id) {
+                Pacote pacote = (Pacote) pacotes.get(posicao);
+                vaiParaResumoPacotes(pacote);
+            }
+        });
+    }
+
+    private void vaiParaResumoPacotes(Pacote pacote) {
+        Intent intent = new Intent(ListaPacotesActivity.this, ResumoPacoteActivity.class);
+        intent.putExtra(CHAVE_PACOTE, pacote);
+        startActivity(intent);
     }
 }
